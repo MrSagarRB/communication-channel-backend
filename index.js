ee691baf4f6ab0d6dd38e804e853dbb345ec2545
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2023-01-22 20:50:57 +0530

    server

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const { json } = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json());
 
+const onlineUsers = [];
+
 // Database Connection
 mongoose.connect(
   "mongodb+srv://sagar:sai@cluster0.ycrzv.mongodb.net/comunication-channel?retryWrites=true&w=majority",
@@ -19,8 +21,35 @@ mongoose.connect(
   }
 );
 
+// Socket
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "https://communication-channel2.vercel.app",
+  },
+});
+
+io.on("connection", (socket) => {
+  //when ceonnect
+
+  socket.on("user_online", (data) => {
+    if (onlineUsers.includes(data)) {
+    } else {
+      onlineUsers.push(data);
+    }
+  });
+
+  socket.on("sendMsg", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_msg", data);
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("Server is running...");
+});
+
+app.get("/getOnlineUsers", (req, res) => {
+  res.send(onlineUsers);
 });
 
 app.get("/getAllUser", async (req, res) => {
@@ -31,7 +60,6 @@ app.get("/getAllUser", async (req, res) => {
 
 app.post("/getUserByID", async (req, res) => {
   await UserModel.find({ _id: req.body.token }).then((result) => {
-    console.log(req.body.token);
     res.send(result);
   });
 });
@@ -58,7 +86,6 @@ app.post("/findChatByID", (req, res) => {
 });
 
 app.post("/createNewChat", (req, res) => {
-  console.log(req.body);
   // res.send(req.body);
 
   let create = new ChatsModel(req.body);
@@ -66,8 +93,6 @@ app.post("/createNewChat", (req, res) => {
 });
 
 app.post("/sendMessage", async (req, res) => {
-  console.log(req.body);
-
   await ChatsModel.updateOne(
     { _id: req.body.id },
     {
@@ -91,5 +116,5 @@ app.get("/getAllMesaages", (req, res) => {
 });
 
 app.listen(3005, () => {
-  console.log("Server is Running on port 3000");
+  console.log("Server is Running on port 3005");
 });
